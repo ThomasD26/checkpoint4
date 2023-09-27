a4765549e854ae977b16224639c301de1f2389e8
ThomasD26 <thomas@ThomasD>
2023-09-27 09:43:04 +0200

    ajustement des routes

--- tchatonline/backend/src/router.js
+++ tchatonline/backend/src/router.js
@@ -6,8 +6,8 @@ const userControllers = require("./controllers/userControllers");
 
 router.get("/user", userControllers.getProfile);
 router.get("/users/:id", userControllers.read);
-router.put("/users/:id", hashPassword, userControllers.edit);
-router.post("/users", checkUserMiddleware, hashPassword, userControllers.add);
+router.put("/users/:id", userControllers.edit);
+router.post("/users", userControllers.add);
 router.get("/user/:userId", userControllers.getUserById);
 
 module.exports = router;
